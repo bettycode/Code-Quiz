49aea365f4f0869d039ef7bc0bc91370a4b97a57
Bethlehem Balcha <betty.haile.us@gmail.com>
2020-10-03 23:09:40 -0400

    modified some js code line

--- quiz.js
+++ quiz.js
@@ -1,10 +1,4 @@
 //declear variables
-var currentQuestions = 0
-
-
-
-
-
 var highscoresLinkEl =document.querySelector("#Highscores-link");
 var timerEl = document.querySelector("#timer");
 var startPageEl = document.querySelector("#start-page");
@@ -12,55 +6,58 @@ var btnStartEl = document.querySelector("#start");
 var questionsPgEl = document.querySelector("#questions-pg");
 var questionTextEl =document.querySelector("#question-text");
 var choicesEl = document.querySelector("#choices");
-var allDoneEl = document.querySelector("#All-done");
-var highscoreEl = document.querySelector("#highscore")
-var resetBtnEl = document.querySelector("#goback");
-var clearEl = document.querySelector("#clear highscore");
+//var allDoneEl = document.querySelector("#All-done");
+var highscoreEl = document.querySelector("#highscore");
+//var resetBtnEl = document.querySelector("#goback");
+//var clearEl = document.querySelector("#clear highscore");
 var btnChoiceEl = document.getElementsByClassName("choise");
+var questionIndex= 0;
 
-
-var currentQuestions = {};
-
+var answer = true;
+var choiceIndex = 0;
 //creat the rest of the questions and answers.
 
 var questionEl = [
     {
-        asked: 'Commonly used data types DO NOT include:',
+        question: 'Commonly used data types DO NOT include:',
         choice: ['strings', 'booleans', 'alerts', 'numbers'],
         answer: 'alerts'
     
     },
 
     { 
-        asked:  'The condition in an if / else statement is enclosed within ____.',
+        question:  'The condition in an if / else statement is enclosed within ____.',
         choice: ['quotes', 'curly brackets', 'parentheses', 'square brackets'],
         answer: 'parentheses'
     },
 
     {
-        asked: 'Arrays in JavaScript can be used to store ____.',
+        question: 'Arrays in JavaScript can be used to store ____.',
         choice: ['numbers and strings', 'other arrays', 'booleans', 'all of the above'],
         answer: 'all of the above'   
     },
 
     {
-        asked: 'String values must be enclosed within ____ when being assigned to variables.',
+        question: 'String values must be enclosed within ____ when being assigned to variables.',
         choice: ['commas', 'curly brackets', 'quotes', 'parentheses'],
         answer: 'quotes'
     },
 
     {
-        asked: 'A very useful tool used during development and debugging for printing content to the debugger is:',
+        question: 'A very useful tool used during development and debugging for printing content to the debugger is:',
         choice: ['Javascript', 'terminal / bash', 'for loops', 'console log'],
         answer: 'console log'
     }
 
 ];
 
+
+
 //start timer, show quiz and hide start button and other texts .
 
 var timer = 60;
 var score= 0
+
 //display questions and timer
 function showQuiz(){
     questionsPgEl.style.display = "block"; //question displayed.
@@ -83,15 +80,62 @@ function showQuiz(){
 
 }
 
-
+var currentQuestions;
+var currentChoice;
 //loop the questions when answer is clicked.
 function displayQuestions(){
     
+    var currentQuestions= questionEl[i].question;
+    currentQuestions.question=questionTextEl.textContent ;
+    var currentChoice= questionEl[i].choice;
+    btnChoiceEl.textContent = currentChoice.choice;
     
-    //getNewQuestions();
-}    
+    
+    
+    //console.log(choiceIndex);
+    //console.log(questionIndex);
+    //console.log(currentQuestions);
+    //console.log(currentChoice);
+    //console.log(questionEl[1].question);
+    //console.log(questionEl[1].choice);
+    console.log(questionIndex[0]);
+    console.log(questionIndex[1]);
+
+    console.log(questionIndex[2]);
 
 
+}
+
+    
+
+    for (var i = 0; i< questionTextEl.length; i++){
+        var questionIndex = Math.floor(Math.random()*questionTextEl.length);
+        console.log(questionIndex);
+        
+
+        btnChoiceEl.addEventListener("click",()=>{
+            if (answer===true){
+                currentQuestions=questionEl[1].question;
+                currentChoice = questionEl[1].choice;
+                questionIndex++
+                choiceIndex++
+    
+            }
+            else{
+                currentQuestions=questionEl[1].question;
+                currentChoice = questionEl[1].choice;
+            }
+            nextQuiz();
+    
+        });   
+
+        //console.log( questionIndex++)
+
+
+        console.log("=========")
+    }
+
+    
 //when done with the quiz hide the current page and display all done page.
 //show final score and ask for initials and store the scores.
 //show high scores.
@@ -104,3 +148,5 @@ btnStartEl.addEventListener("click",()=>{
 
 }
 );
+    
+
